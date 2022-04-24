cb4c78470db516054935779656e3b4f8ddd56085
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-04-24 23:33:56 +0530

    Responsive UI + Map

---
+++ js/scripts.js
@@ -0,0 +1,73 @@
+
+
+window.addEventListener('DOMContentLoaded', event => {
+
+    // Navbar shrink function
+    var navbarShrink = function () {
+        const navbarCollapsible = document.body.querySelector('#mainNav');
+        if (!navbarCollapsible) {
+            return;
+        }
+        if (window.scrollY === 0) {
+            navbarCollapsible.classList.remove('navbar-shrink')
+        } else {
+            navbarCollapsible.classList.add('navbar-shrink')
+        }
+
+    };
+
+    // Shrink the navbar 
+    navbarShrink();
+
+    // Shrink the navbar when page is scrolled
+    document.addEventListener('scroll', navbarShrink);
+
+    // Activate Bootstrap scrollspy on the main nav element
+    const mainNav = document.body.querySelector('#mainNav');
+    if (mainNav) {
+        new bootstrap.ScrollSpy(document.body, {
+            target: '#mainNav',
+            offset: 74,
+        });
+    };
+
+    // Collapse responsive navbar when toggler is visible
+    const navbarToggler = document.body.querySelector('.navbar-toggler');
+    const responsiveNavItems = [].slice.call(
+        document.querySelectorAll('#navbarResponsive .nav-link')
+    );
+    responsiveNavItems.map(function (responsiveNavItem) {
+        responsiveNavItem.addEventListener('click', () => {
+            if (window.getComputedStyle(navbarToggler).display !== 'none') {
+                navbarToggler.click();
+            }
+        });
+    });
+
+});
+
+$(document).ready(function(){
+    $(".hide").hide();
+    
+    
+    $(".marker-russia").click(function(){
+      $(".hide").hide();
+      $(".russia-txt").fadeIn(300);
+    });
+    
+      $(".marker-usa").click(function(){
+         $(".hide").hide();
+      $(".usa-txt").fadeIn(300);
+    });
+    
+      $(".marker-uk").click(function(){
+         $(".hide").hide();
+      $(".uk-txt").fadeIn(300);
+    });
+    
+      $(".marker-brazil").click(function(){
+         $(".hide").hide();
+      $(".brazil-txt").fadeIn(300);
+    });
+  
+  });
