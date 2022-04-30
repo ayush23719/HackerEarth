badf2453664bc3a6147b683fef5e11a18241f537
ayush23719 <91633771+ayush23719@users.noreply.github.com>
2022-04-30 20:06:55 +0530

    Americas Card

--- js/scripts.js
+++ js/scripts.js
@@ -45,29 +45,3 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
-
-// $(document).ready(function(){
-//     $(".hide").hide();
-    
-    
-//     $(".marker-russia").click(function(){
-//       $(".hide").hide();
-//       $(".russia-txt").fadeIn(300);
-//     });
-    
-//       $(".marker-usa").click(function(){
-//          $(".hide").hide();
-//       $(".usa-txt").fadeIn(300);
-//     });
-    
-//       $(".marker-uk").click(function(){
-//          $(".hide").hide();
-//       $(".uk-txt").fadeIn(300);
-//     });
-    
-//       $(".marker-brazil").click(function(){
-//          $(".hide").hide();
-//       $(".brazil-txt").fadeIn(300);
-//     });
-  
-//   });
